487529d443a99ca6a186f37c0df3aef607957e36
Nitin Mohapatra <nitinmohapatra26@gmail.com>
2026-04-28 13:14:04 +0530

    Added MessageItem and replace the some code part in ProjectChat and added the messageItem

--- Frontend/src/Components/ProjectChat.jsx
+++ Frontend/src/Components/ProjectChat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useCallback , memo } from "react";
 import axiosInstance from "../utils/axiosInstance"
 import { io } from "socket.io-client";
 import { jwtDecode } from "jwt-decode";
